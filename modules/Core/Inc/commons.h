933fb8039e4367487a2daf011b7d77003453b186
leochams <72444888+leochams@users.noreply.github.com>
2023-09-03 14:56:15 +0200

    Add Core file

---
+++ modules/Core/Inc/commons.h
@@ -0,0 +1,15 @@
+/*
+ * commons.h
+ *
+ *  Created on: Aug 31, 2023
+ *      Author: leoch
+ */
+
+#ifndef INC_COMMONS_H_
+#define INC_COMMONS_H_
+
+#include "BME280.h"
+
+BME280 BME280_sensor;
+
+#endif /* INC_COMMONS_H_ */
